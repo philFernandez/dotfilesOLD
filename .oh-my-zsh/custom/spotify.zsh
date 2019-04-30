65151f6c053ea515a8ea6d56bc5f4545bfb17e65
philFernandez <norcal618@gmail.com>
2019-04-29 22:13:45 -0700

    alias file for spotify

---
+++ .oh-my-zsh/custom/spotify.zsh
@@ -0,0 +1,13 @@
+# Sublime
+alias sublime='spotify play list Sublime'
+alias santeria='spotify play uri spotify:track:2hnMS47jN0etwvFPzYk11f'
+alias whatigot='spotify play spotify:track:6UmtL9oAPxl4ixd0YKtKnH'
+alias wrongway='spotify play spotify:track:6uUqBB6wXrcIJWssTRfIYE'
+alias 40oz='spotify play uri spotify:track:6TdySkLFeLQzZnesrSXvjr'
+alias twojoints='spotify play uri spotify:track:0FdTBwYr1aNF1smfqaoCde'
+alias ronjeremy='spotify play uri spotify:track:194iHn32nKcqWH8TOLWaov'
+alias gardengrove='spotify play uri spotify:track:0aI3sbV4I4TVoA'
+alias daterape='spotify play uri spotify:track:0ManjhaoAvBeijzAqTmSK3'
+# Commands
+alias next='spotify next'
+alias pause='spotify pause'
