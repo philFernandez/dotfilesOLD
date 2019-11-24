bd6a72b0230a5cb33f2341e27a63eb9b67fe5990
Phil Fernandez <norcal618@gmail.com>
2019-11-24 03:47:10 -0800

    new fold Z_aliases

--- .oh-my-zsh/custom/Z_aliases.zsh
+++ .oh-my-zsh/custom/Z_aliases.zsh
@@ -1,27 +1,17 @@
-# Temp Aliases --------------------------------------
+# Temp Aliases {{{1
 alias b='(cd ~/eclipse-workspace/A3Prj && ant jar)'
-# ---------------------------------------------------
-alias github='ln -sf ~/.gitconfigs/.gitconfig.github ~/.gitconfig'
-alias gitlab='ln -sf ~/.gitconfigs/.gitconfig.gitlab ~/.gitconfig'
-alias glor='LESS+=-N git log --oneline --decorate --reverse'
-alias nl='nl -b a'
-alias fhw='shw | fzf'
-alias v='/usr/local/bin/vim'
-alias cds='fcd ~/Fall_2019'
-alias cla='cls;ls -A'
+
+# Commands and Shortcuts {{{1
 alias remoff='/bin/mv -f ~/.reminders ~/.reminders.silent'
 alias remon='/bin/mv -f ~/.reminders.silent ~/.reminders'
-alias dos2unix="perl -pi -e 's/\r\n$/\n/g'"
-alias bat="bat --theme 'Monokai Extended Bright'"
-alias unix2dos="perl -pi -e 's/$/\r/'"
-alias wtf='wtf -o'
-alias vscode='code -n .'
-alias cpd='pwd|pbcopy'
-alias regex='man pcresyntax'
-alias timestamp='date +"%m/%d/%Y @ %I:%M %p"'
 alias light='iterm2_profile Light'
 alias dark='iterm2_profile Default'
+alias ppath='print -l $path'
+alias server='ssh philthy@192.168.0.36'
+alias timestamp='date +"%m/%d/%Y @ %I:%M %p"'
+alias regex='man pcresyntax'
 alias which='which -a'
+alias tree='lsd --tree --group-dirs first'
 alias cls=clear
 alias c=clear
 alias rm='rm -v'
@@ -29,26 +19,32 @@ alias cll='clear;pwdc;ls -A'
 alias clll='clear;pwdc;ls -Al'
 alias q='exit'
 alias reload='exec zsh'
-alias tree='lsd --tree --group-dirs first'
-alias bk='cd ..;cl'
 alias vi='vim'
 alias mv='mv -vi'
 alias cp='cp -vip'
-alias ppath='print -l $path'
+alias vi='vim'
+alias mv='mv -vi'
+alias cp='cp -vip'
 alias ds='du -sh'
-alias server='ssh philthy@192.168.0.36'
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias wiki='v -c VimwikiIndex'
-alias wiki2='v ~/Nextcloud/CSUS/Fall_2019/wiki/index.wiki'
-alias chkbu='watch -n 0.1 ls -all /Volumes'
-alias selbook='open $(mdfind -onlyin $HOME/Spring_2019 -interpret "pdf" | sk -m)'
+alias nl='nl -b a'
+alias v='/usr/local/bin/vim'
+alias cla='cls;ls -A'
+
+
+# Bat & Fzf {{{1
+alias bat="bat --theme 'Monokai Extended Bright'"
+alias fhw='shw | fzf'
 alias fzfbat='fd -tf --ignore-file $HOME/.cust_ignore | fzf --preview \
 	"bat --theme=Monokai\ Extended\ Bright \
 	--style=numbers,changes --color always {}" \
 	--preview-window="down:95%"'
 
-# dotfile/config bar repo aliases
+# Git {{{1
+alias github='ln -sf ~/.gitconfigs/.gitconfig.github ~/.gitconfig'
+alias gitlab='ln -sf ~/.gitconfigs/.gitconfig.gitlab ~/.gitconfig'
+alias glor='LESS+=-N git log --oneline --decorate --reverse'
+
+# Version Controled Dotfiles {{{1
 alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias cst='config status'
 alias ca='config add'
@@ -59,10 +55,10 @@ alias clog='config log --oneline --decorate --graph --all'
 alias configls='config ls-tree -r master --name-only $HOME'
 alias cdl='config diff | bat --style=numbers -l diff'
 
-# make man pages display line numbers
+# make man pages display line numbers {{{1
 alias man='LESS+="-N" man'
 
-# Package Management
+# Package Management {{{1
 alias update='brew -v update && brew outdated && brew cask outdated'
 alias upgrade='brew upgrade && brew cask upgrade'
 alias outdated='brew -v outdated && brew cask outdated'
@@ -70,3 +66,6 @@ alias dryclean='brew cleanup --dry-run'
 alias clean='brew cleanup -s'
 alias brews='brew list -1'
 alias show='brew info'
+
+# {{{1
+# vim:foldenable foldmethod=marker foldcolumn=1
