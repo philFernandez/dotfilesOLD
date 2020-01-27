21264abf649894f112cf30aedeef3c500cd522b1
philFernandez <norcal618@gmail.com>
2020-01-27 15:49:03 -0800

    custom colored-man-pages

---
+++ .zsh/colored-man-pages-custom.zsh
@@ -0,0 +1,19 @@
+# Same as oh-my-zsh plugin, but colors changed
+function colored() {
+  command env \
+    LESS_TERMCAP_mb=$(printf "\e[1;32m") \
+    LESS_TERMCAP_md=$(printf "\e[1;32m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[01;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;4;31m") \
+    PAGER="${commands[less]:-$PAGER}" \
+    _NROFF_U=1 \
+    PATH="$HOME/bin:$PATH" \
+    "$@"
+  }
+
+function man() {
+  colored man "$@"
+}
